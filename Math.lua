fd00c9546f800d65b34fb333c0ac416edf58d167
AkiKonani <aki.konani@gmail.com>
2022-11-24 15:52:25 +0000

    Removal of dependencies to GMR

--- Math.lua
+++ Math.lua
@@ -1,31 +1,42 @@
 local function add(a, b)
-    return a + b
+  return a + b
 end
 
 local function sum(list)
-    return Array.reduce(list, add, 0)
+  return Array.reduce(list, add, 0)
 end
 
 local function multiply(a, b)
-    return a * b
+  return a * b
 end
 
 local function product(list)
-    return Array.reduce(list, multiply, 1)
+  return Array.reduce(list, multiply, 1)
 end
 
 local function round(value, numberOfDigitsAfterTheDecimalSeparator)
-    if numberOfDigitsAfterTheDecimalSeparator == nil then
-       numberOfDigitsAfterTheDecimalSeparator = 0
-    end
-    local a = 10^ numberOfDigitsAfterTheDecimalSeparator
-    return math.floor(value * a + 0.5) / a
+  if numberOfDigitsAfterTheDecimalSeparator == nil then
+    numberOfDigitsAfterTheDecimalSeparator = 0
+  end
+  local a = 10 ^ numberOfDigitsAfterTheDecimalSeparator
+  return math.floor(value * a + 0.5) / a
+end
+
+local function sign(value)
+  if value < 0 then
+    return -1
+  elseif value == 0 then
+    return 0
+  else
+    return 1
+  end
 end
 
 Math = {
-    add = add,
-    sum = sum,
-    multiply = multiply,
-    product = product,
-    round = round
+  add = add,
+  sum = sum,
+  multiply = multiply,
+  product = product,
+  round = round,
+  sign = sign
 }
